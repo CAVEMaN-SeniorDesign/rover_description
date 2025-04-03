0d24dbd8621d4e3cdc61654ccffc84cbbccf6033
DZZLab <david.zhang@pitt.edu>
2025-04-03 19:16:32 +0000

    Removed sim related things

--- launch/rover.launch.py
+++ launch/rover.launch.py
@@ -4,6 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import Command
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, TimerAction
 
 # this is the function launch  system will look for
 def generate_launch_description():
@@ -12,25 +13,27 @@ def generate_launch_description():
     print("Fetching URDF ==>")
     #robot_desc_path = os.path.join(get_package_share_directory(package_description), "rover_desc", urdf_file)
     urdf_file = os.path.join(get_package_share_directory(package_description), 'rover_desc', 'rover.xacro')    #xacro_file = "urdfbot.xacro"
-    
+    urdf_content = Command(['xacro ', urdf_file])
 
     # Robot State Publisher
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        emulate_tty=True,
-        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', urdf_file])}],
+        parameters=[{
+            'use_sim_time': True, 
+            'robot_description': urdf_content,
+        }],
         output="screen"
     )
     
-    #joint state publisher
-    joint_state_publisher_node = Node(
-    package='joint_state_publisher',
-    executable='joint_state_publisher',
-    name='joint_state_publisher',
-    parameters=[{'use_sim_time': True}],
-    output="screen"
+    #joint state publisher: Publishes fake or gui-driven msgs for testing (joint_state_publisher_gui joint_state_publisher_gui)
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        parameters=[],        
+        output="screen"
     )
 
 
@@ -38,6 +41,6 @@ def generate_launch_description():
     return LaunchDescription(
         [            
             robot_state_publisher_node,
-            joint_state_publisher_node
+            joint_state_publisher_gui_node,
         ]
     )
